da2332a2be6023d7010a5370228839e4fc838bab
André Lucas <andre.lucas97@outlook.com>
2024-03-11 17:24:50 -0300

    att-03-11
    Atualizacao 11/03

--- ByteBankIO/ByteBankIO/Program.cs
+++ ByteBankIO/ByteBankIO/Program.cs
@@ -4,7 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
+        var enderecoArquivo = "contas.txt";
+        var numeroByteLidos = -1;
+
+        var fluxoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024];
+        
+
+        while (numeroByteLidos != 0)
+        {
+            numeroByteLidos = fluxoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+
+        }
+
+        fluxoArquivo.Read(buffer, 0, 1024);
 
         Console.ReadLine();
+    }
+
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        foreach (var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
     }
 }
